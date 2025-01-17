56e6956707a2705c08ff30fdf76137abde7e1c8a
yassineedev <yassine.boussif18@gmail.com>
2025-01-17 15:56:14 +0100

    ADD ROUTERS CONFIG FOR VUE JS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import './assets/main.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import { router } from './routers'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from "pinia"
-
+App.use(router)
 createApp(App).use(createPinia()).mount('#app')
