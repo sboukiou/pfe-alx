263e77783c7c5a23e5f19a1d0b9614405f0390e1
yassineedev <yassine.boussif18@gmail.com>
2025-01-18 00:54:56 +0100

    add navbar compenents for landing page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="">
+                    <a class="navbar-brand">
+                        <h4 class="title-nav">Todo <span class="title-span">List</span></h4>
+                    </a>
+                    
+                </div>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="nav ">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                      </ul>
+                </div>
+                <div>
+                    <button class="btn btn-primary" type="button">Try it Now!!</button>
+                </div>
+                <div>
+                </div>
+            </div>
+    </nav>
+</template>
+<script>
+</script>
