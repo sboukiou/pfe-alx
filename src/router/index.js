d048f25b0731967573faa9133a54020226d51981
yassineedev <yassine.boussif18@gmail.com>
2025-01-17 16:57:39 +0100

    adding imports to app.vue but nothing working for this time

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TasksApp from '@/views/TasksApp.vue'
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-        {
-            path: '/taskapp',
-            name: 'tasksapp',
-            component: TasksApp,
-        },
-    ],
-})
+// const router = createRouter({
+//     history: createWebHistory(import.meta.env.BASE_URL),
+//     routes: [
+//         {
+//             path: '/',
+//             name: 'home',
+//             component: HomeView,
+//         },
+//         {
+//             path: '/taskapp',
+//             name: 'tasksapp',
+//             component: TasksApp,
+//         },
+//     ],
+// })
 
-export default router
+// export default router
+
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
--- src/views/TasksApp.vue
+++ src/views/TasksApp.vue
@@ -1,6 +1,45 @@
 <template>
-    <h1>tasks app page</h1>
-</template>
-<script setup>
+      <main>
+    <header>
+      <img src="../public/img/logo.png" alt="pinia logo">
+      <h1>{{TaskStore.name}} Tasks</h1>
+    </header>
+    <!-- new task form -->
+     <div class="new-task-form">
+      <TaskForm />
+     </div>
+    <!-- filter -->
+     <nav class="filter">
+        <button @click="filter = 'all'">All tasks</button>
+        <button @click="filter = 'favs'">Fav tasks</button>
+     </nav>
+    <!-- tasks list -->
+     <div class="task-list" v-if="filter === 'all'">
+      <p>All tasks: {{ TaskStore.totalCounter }}</p>
+      <div v-for="task in TaskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+     </div>
 
+     <div class="task-list" v-if="filter === 'favs'">
+      <p>Fav Tasks: {{ TaskStore.isFavCounter }}</p>
+      <div v-for="task in TaskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+     </div>
+  </main>
+</template>
+<script >
+import TaskDetails from "../components/TaskDetails.vue";
+import TaskForm from "../components/TaskForm.vue";
+import { useTaskStore } from '../stores/TaskStore';
+import { ref } from "vue";
+  export default {
+    components: {TaskDetails, TaskForm},
+    setup (){
+      const TaskStore = useTaskStore()
+      const filter = ref('all')
+      return { TaskStore,filter }
+      }
+  }
 </script>
